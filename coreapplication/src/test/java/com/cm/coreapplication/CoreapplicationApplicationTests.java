a9eb8525d2c51695c2fae26701d1edc0209f8e63
Khan, C M Abdullah <cmabdullah21@gmail.com>
2024-01-27 23:40:26 +0600

    core

---
+++ coreapplication/src/test/java/com/cm/coreapplication/CoreapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cm.coreapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
